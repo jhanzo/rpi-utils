ba74e2c208c983a8e28b204ab8e268d76891bc1d
jhanzo <jhanzo@users.noreply.github.com>
2016-10-16 09:38:53 +0000

    add dockerfiles

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 #download Docker for Raspberry
 curl -sSL https://get.docker.com | sh
+
+#add pi user to docker group
+sudo usermod -aG docker pi
+
+#enable docker service
+sudo systemctl enable docker.service
